96e8824d46e6b4d4175e5d8024a222b9c4410d73
SveOls <sverre.d.olsen@live.no>
2024-02-04 12:14:25 +0100

    h

--- fire.lua
+++ fire.lua
@@ -2,9 +2,51 @@ local logslot = 1
 local stickslot = 2
 local saplingslot = 3
 local appleslot = 4
+local pieceslot = 5
 local fuelslot = 16
 os.loadAPI("tortoise")
 while true do
-    
+    if turtle.getFuelLevel() < 200 then
+        turtle.select(fuelslot)
+        tortoise.refuel()
+    end
+    turtle.suck()
+    turtle.suck()
+    turtle.suck()
+    tortoise.turnRight()
+    tortoise.forward()
+    tortoise.setReturn()
+    tortoise.up()
+    tortoise.up()
+    tortoise.forward()
+    turtle.select(logslot)
+    local logc = turtle.getItemCount()
+    turtle.dropDown(logc - 1)
+    tortoise.goReturn()
+    tortoise.forward()
+    turtle.suckUp()
+    turtle.select(pieceslot)
+    local piecec = turtle.getItemCount()
+    turtle.craft(56 - pieceslot)
+    piecec = turtle.getItemCount()
+    if piecec > 1 then
+        turtle.dropUp(piecec - 1)
+    end
+    tortoise.turnLeft()
+    for i=1,16 do
+        turtle.select(i)
+        if i == logslot or i == stickslot or i == saplingslot or i == pieceslot or i == fuelslot then
+            local c = turtle.getItemCount()
+            if c > 1 then
+                turtle.drop(c - 1)
+            end
+        else
+            turtle.drop()
+        end
+    end
+    tortoise.turnLeft()
+    tortoise.forward()
+    tortoise.forward()
+    tortoise.turnRight()
     os.sleep(10)
 end
